680cd37529b4f85856ad240f24c70d7513febc9d
daoduytung2209 <daoduytung2209@gmail.com>
2018-12-13 14:44:15 +0800

    Test

--- app/src/main/java/com/example/user/cp3405designthinking/MainActivity.java
+++ app/src/main/java/com/example/user/cp3405designthinking/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private boolean iii;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
