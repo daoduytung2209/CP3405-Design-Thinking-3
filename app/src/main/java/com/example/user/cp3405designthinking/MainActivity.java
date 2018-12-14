1c4b21b0884964d8f85aa4f1b1c21e33da7c48e6
daoduytung2209 <daoduytung2209@gmail.com>
2018-12-15 00:15:31 +0800

    test branch

--- app/src/main/java/com/example/user/cp3405designthinking/MainActivity.java
+++ app/src/main/java/com/example/user/cp3405designthinking/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private boolean iii;
+    private boolean tung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
